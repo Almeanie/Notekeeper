e1a5967516d1c63f381ca476dfd3dc7878fb82b1
Almeanie <93413927+Almeanie@users.noreply.github.com>
2025-02-08 19:43:27 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+        <Header />
+        <Note />
+        <Note />
+        <Note />
+        <Footer />
+        </div>
+    );
+}
+
+export default App;
